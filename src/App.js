97d76ae67a82fc38a25f4e31814ee7e7b64b4e7d
cmpradeepkumar <cmpradeepkumar@gmail.com>
2020-10-01 20:45:25 -0300

    2 files modified

--- src/App.js
+++ src/App.js
@@ -8,32 +8,36 @@ export default function App() {
     console.log('addItem');
   }
   return (
-    <div class="base">
-    <div class="calcResult">{result}</div>
-    <div class="calcTable">      
-      <div class="calcRow">
-        <div class="calcCell"><button onClick={addItem}>0</button></div>
-        <div class="calcCell"><button onClick={addItem}>1</button></div>
-        <div class="calcCell"><button onClick={addItem}>2</button></div>
-        <div class="calcCell"><button onClick={addItem}>+</button></div>
+    <div className="base">
+    <div className="calcResult">{result}</div>
+    <div className="calcTable">      
+      <div className="calcRow">
+        <div className="calcCell"><button onClick={addItem} className="buttonBlue">7</button></div>
+        <div className="calcCell"><button onClick={addItem} className="buttonBlue">8</button></div>
+        <div className="calcCell"><button onClick={addItem} className="buttonBlue">9</button></div>
+        <div className="calcCell"><button onClick={addItem} className="buttonOrange">+</button></div>
       </div>
-      <div class="calcRow">
-        <div class="calcCell"><button onClick={addItem}>3</button></div>
-        <div class="calcCell"><button onClick={addItem}>4</button></div>
-        <div class="calcCell"><button onClick={addItem}>5</button></div>
-        <div class="calcCell"><button onClick={addItem}>-</button></div>
+      <div className="calcRow">
+        <div className="calcCell"><button onClick={addItem} className="buttonBlue">4</button></div>
+        <div className="calcCell"><button onClick={addItem} className="buttonBlue">5</button></div>
+        <div className="calcCell"><button onClick={addItem} className="buttonBlue">6</button></div>
+        <div className="calcCell"><button onClick={addItem} className="buttonOrange">-</button></div>
       </div>
-      <div class="calcRow">
-        <div class="calcCell"><button onClick={addItem}>6</button></div>
-        <div class="calcCell"><button onClick={addItem}>7</button></div>
-        <div class="calcCell"><button onClick={addItem}>8</button></div>
-        <div class="calcCell"><button onClick={addItem}>*</button></div>
+      <div className="calcRow">
+        <div className="calcCell"><button onClick={addItem} className="buttonBlue">1</button></div>
+        <div className="calcCell"><button onClick={addItem} className="buttonBlue">2</button></div>
+        <div className="calcCell"><button onClick={addItem} className="buttonBlue">3</button></div>
+        <div className="calcCell"><button onClick={addItem} className="buttonOrange">*</button></div>
       </div>
-      <div class="calcRow">
-        <div class="calcCell"><button onClick={addItem}>9</button></div>
-        <div class="calcCell"><button onClick={addItem}>/</button></div>
-        <div class="calcCell"><button onClick={addItem}>=</button></div>
-        <div class="calcCell"><button onClick={addItem}>c</button></div>
+      <div className="calcRow">
+        <div className="calcCell"><button onClick={addItem} className="buttonLarge buttonRed">0</button></div>
+        <div className="calcCell"><button onClick={addItem} className="buttonBlue">.</button></div>        
+        <div className="calcCell"><button onClick={addItem} className="buttonOrange">/</button></div>
+      </div>
+      <div className="calcRow">        
+        <div className="calcCell"><button onClick={addItem} className="buttonLarge buttonRed">Del</button></div>
+        <div className="calcCell"><button onClick={addItem} className="buttonOrange">%</button></div>
+        <div className="calcCell"><button onClick={addItem} className="buttonRed">=</button></div>
       </div>
     </div>
     </div>
